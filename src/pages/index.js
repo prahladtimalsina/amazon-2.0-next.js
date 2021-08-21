0d57e6ee3cde75aab0430558adefb002346df260
prahladtimalsina <prahladtimalsina7@gmail.com>
2021-08-21 22:42:18 +0545

    amazone clone starter templet form sonny sanga tempelte

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Amazon 2.0 | Prahlad-Timalsina</title>
+      </Head>
+          <h1>Hey prahlad Timalsina</h1>
+    </div>
+  );
+}
