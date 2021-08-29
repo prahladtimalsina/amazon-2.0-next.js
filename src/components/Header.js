ee4a3ebffb9224b4279e41314de5cc5dec44d917
prahladtimalsina <prahladtimalsina7@gmail.com>
2021-08-29 16:43:11 +0545

    search bar header custom done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,36 @@
 import Image from "next/image";
+import {
+  MenuIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/outline";
 function Header() {
   return (
     <header>
-      <div>
+      <div
+        className="flex items-center bg-amazon_blue p-1
+      flex-grow py-2
+      "
+      >
         {/* top */}
-        <div>
-          <Image src="/static/images/amazon-logo.png"
-           width={150}  height={40}
-           
-           
-           />
+        <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
+          <Image
+            src="/static/images/amazon-logo.png"
+            width={150}
+            height={40}
+            objectFit="contain"
+            className="cursor-pointer"
+          />
+        </div>
+        <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500">
+          <input
+            className="p-2 h-full
+           w-6 flex-grow flex-shrink rounded-l-md focus-outline-none px-4"
+            type="text"
+            name=""
+            id=""
+          />
+          <SearchIcon className="h-12 p-4" />
         </div>
       </div>
 
