6d0ad74c6b9968729eb28dd88a1b5aecd73248cc
prahladtimalsina <prahladtimalsina7@gmail.com>
2021-08-30 15:21:17 +0545

    prduct feed page use slice to render the product, use row-dense and day 1 complete

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -3,8 +3,9 @@ import Product from "./Product";
 function ProductFeed({ products }) {
   return (
     <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto">
-      {products.map(
-        ({ id, title, img, price, description, category, image }) => (
+      {products
+        .slice(0, 4)
+        .map(({ id, title, img, price, description, category, image }) => (
           <Product
             key={id}
             title={title}
@@ -14,9 +15,41 @@ function ProductFeed({ products }) {
             category={category}
             image={image}
           />
-        )
-      )}
-    </div>  
+        ))}
+      <img
+        className="md:col-span-full"
+        src="/static/images/banner-col.jpg"
+        alt=""
+      />
+      <div className="md:col-span-2">
+        {products
+          .slice(4, 5)
+          .map(({ id, title, img, price, description, category, image }) => (
+            <Product
+              key={id}
+              title={title}
+              img-={img}
+              price={price}
+              description={description}
+              category={category}
+              image={image}
+            />
+          ))}
+      </div>
+      {products
+        .slice(5,products.length)
+        .map(({ id, title, img, price, description, category, image }) => (
+          <Product
+            key={id}
+            title={title}
+            img-={img}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+          />
+        ))}
+    </div>
   );
 }
 
