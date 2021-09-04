263c4d90f0f473a0fe80acef803559fb2515001d
prahladtimalsina <prahladtimalsina7@gmail.com>
2021-09-04 09:36:56 +0545

    selected items pull from the gloabl store useSelect hooks used to fetch items

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,10 +6,13 @@ import {
 } from "@heroicons/react/outline";
 import { signIn, signOut, useSession } from "next-auth/client";
 import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
+import { selectItems } from "../slices/basketSlice";
 function Header() {
   const [session] = useSession();
   // console.log(session.user);
   const router = useRouter();
+  const items = useSelector(selectItems);
   return (
     <header className="sticky z-50 top-0">
       <div
@@ -56,7 +59,7 @@ function Header() {
             className="link relative flex items-center "
           >
             <span className="absolute top-0 right-0 md:right-10 h-4 w-4  bg-yellow-400 text-center rounded-full text-black font-bold">
-              0
+              {items.length}
             </span>
             <ShoppingCartIcon className="h-10" />
             <p className="hidden md:inline font-extrabold md:text-sm mt-2">
