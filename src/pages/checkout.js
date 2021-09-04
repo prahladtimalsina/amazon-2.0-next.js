99bb07c87d195bf2c3ef9fb9f7f3fa7b5c36ef31
prahladtimalsina <prahladtimalsina7@gmail.com>
2021-09-04 09:01:52 +0545

    checkout shoping basket design before readux portion start

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -7,13 +7,16 @@ function Checkout() {
       <Header />
       <main className="lg:flex max-w-screen-2xl mx-auto">
         {/* left */}
-        <div>
+        <div className="flex-grow m-5 shadow-sm">
           <Image
             src="/static/images/Prime-day-banner.png"
             width={1020}
             height={250}
             objectFit="contain"
           />
+          <div className="flex flex-col p-5 space-y-10 bg-white">
+            <h1 className="text-3xl border-b pb-4">Your Shopping Basket</h1>
+          </div>
         </div>
         {/* right */}
       </main>
