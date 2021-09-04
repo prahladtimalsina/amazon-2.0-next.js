5df8ead6f5855f0cda203afc7433761d934503eb
prahladtimalsina <prahladtimalsina7@gmail.com>
2021-09-04 09:26:15 +0545

    dispatch product and add the product to action.payload use redux toolkit action and dispatch used for the gloabl store the product

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import basketReducer from "../slices/basketSlice";
 
+//global store setup
+
 export const store = configureStore({
   reducer: {
     basket: basketReducer,
