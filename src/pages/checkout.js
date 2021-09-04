9e1aa85e4b40caff359ee05a88ec55fae1d7831c
prahladtimalsina <prahladtimalsina7@gmail.com>
2021-09-04 08:56:48 +0545

    checkout page image added src

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,9 +1,22 @@
+import Image from "next/image";
 import Header from "../components/Header";
 
 function Checkout() {
   return (
     <div className="bg-gray-100">
-      <Header />i am checkout page
+      <Header />
+      <main className="lg:flex max-w-screen-2xl mx-auto">
+        {/* left */}
+        <div>
+          <Image
+            src="/static/images/Prime-day-banner.png"
+            width={1020}
+            height={250}
+            objectFit="contain"
+          />
+        </div>
+        {/* right */}
+      </main>
     </div>
   );
 }
