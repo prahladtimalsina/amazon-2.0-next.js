a588fa1846c847124a38367ad89652d8c734d49b
prahladtimalsina <prahladtimalsina7@gmail.com>
2021-09-09 23:17:40 +0545

    create order  pages to fetch the data form cloud firestore and stripe orders

--- firebase.js
+++ firebase.js
@@ -1,4 +1,10 @@
-import firebase from "firebase";
+// import firebase from "firebase";
+// import * as firebase from "firebase/app"
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/storage";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAGQSiy0pAjdENtQ72V1DX8dE-0Euzp49Y",
   authDomain: "amazn-2-nextjs.firebaseapp.com",
@@ -7,3 +13,10 @@ const firebaseConfig = {
   messagingSenderId: "330280725818",
   appId: "1:330280725818:web:c52fd1a70ec5a048663403",
 };
+
+const app = !firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
+
+const db = app.firestore();
+export default db;
